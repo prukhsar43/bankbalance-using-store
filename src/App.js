98227b366f769d3497ae3ff68bf3b77109b954dc
Rukhsar <prukhsar43@gmail.com>
2022-07-08 17:16:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Navbar";
+
+import Show from "./Show";
+
+function App() {
+  return (<>
+  <Navbar/>
+   <Show/>
+
+   </>
+   )
+};
+
+export default App;
